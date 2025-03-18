4de4cc19c849e2da030b593bb8fe108ce16416c0
Nikita <reshetnyaknikita6@gmail.com>
2025-03-18 19:02:34 +0300

    update: Update name of db,migrations

--- makefile
+++ makefile
@@ -1,4 +1,3 @@
-include .env
 LOCAL_BIN:=$(CURDIR)/bin
 
 LOCAL_MIGRATION_DIR=$(MIGRATION_DIR)
---
+++ migrations/20250317165631_user.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE user{
+    id serial primary key,
+    name varchar(255) not null,
+    email varchar(255) unique not null,
+    password varchar(255) not null,
+    password_confirm varchar(255) not null, 
+    role varchar(255) not null
+
+}
+-- +goose Down
+DROP TABLE user;
